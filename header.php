ca68909c181fd58f8dff59313837cb07759dc629
Matthew <mgp271@gmail.com>
2015-09-08 08:31:10 -0700

    Added String Replace to Query String
    var query = location.search.substring(1);
    	query = query.replace(/%20/g," ");
    	query = query.replace(/%21/g,"!");
    	query = query.replace(/%22/g,'"');
    	query = query.replace(/%23/g,"#");
    	query = query.replace(/%24/g,"$");
    	query = query.replace(/%25/g,"%");
    	query = query.replace(/%26/g,"&");
    	query = query.replace(/%27/g,"'");
    	query = query.replace(/%28/g,"(");
    	query = query.replace(/%29/g,")");
    	query = query.replace(/%2A/g,"*");
    	query = query.replace(/%2B/g,"+");
    	query = query.replace(/%2C/g,",");
    	query = query.replace(/%2D/g,"-");
    	query = query.replace(/%2E/g,".");
    	query = query.replace(/%2F/g,"/");

--- header.php
+++ header.php
@@ -8,10 +8,28 @@
 	
 	// show once
 	var query = location.search.substring(1);
+	query = query.replace(/%20/g," ");
+	query = query.replace(/%21/g,"!");
+	query = query.replace(/%22/g,'"');
+	query = query.replace(/%23/g,"#");
+	query = query.replace(/%24/g,"$");
+	query = query.replace(/%25/g,"%");
+	query = query.replace(/%26/g,"&");
+	query = query.replace(/%27/g,"'");
+	query = query.replace(/%28/g,"(");
+	query = query.replace(/%29/g,")");
+	query = query.replace(/%2A/g,"*");
+	query = query.replace(/%2B/g,"+");
+	query = query.replace(/%2C/g,",");
+	query = query.replace(/%2D/g,"-");
+	query = query.replace(/%2E/g,".");
+	query = query.replace(/%2F/g,"/");
+	
 	var date = new Date();
-    var minutes = 30;
-    date.setTime(date.getTime() + (minutes * 60 * 1000));
-	jQuery.cookie("googy", query, { expires: date, path: '/', domain: 'yourdomain.com' });
+        var minutes = 60;
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+	// jQuery.cookie("googy", query, { expires: date, path: '/', domain: 'tvlift.com' });
+	jQuery.cookie("googy", query, { expires: date, path: '/' });
 	
 	<?php endif; ?>
 	
@@ -21,7 +39,7 @@
 	//if (location.search.substring(1).length) {
 	if (jQuery.cookie("googy").length) {
 		//var search_arr = location.search.substring(1).split("&");
-		var search_arr = jQuery.cookie("googy").split("&");
+		//var search_arr = jQuery.cookie("googy").split("&");
 		for (var item in search_arr) {
 			if (search_arr[item].length) { // lose this question if you want to send empty parameters
 				var item_arr = search_arr[item].split("=");
